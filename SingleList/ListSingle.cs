a9a90bd2659a19a1ba26e65f04cb416ba4160726
DiamondAngel1 <tararukolga78@gmail.com>
2024-12-29 20:23:51 +0200

    first commit

---
+++ SingleList/ListSingle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleList{
+    public class Node<T>{
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T data){
+            Data = data;
+            Next = null;
+        }
+    }
+    public class SingleList<T>{
+        private Node<T> head;
+        private int count;
+        public SingleList(){
+            head = null;
+            count = 0;
+        }
+        public void Add(T data){
+            Node<T> newNode = new Node<T>(data);
+            if (head == null){
+                head = newNode;
+            }
+            else{
+                Node<T> current = head;
+                while (current.Next != null){
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            count++;
+        }
+        public bool Remove(T data){
+            if (head == null){
+                return false;
+            }
+            if (head.Data.Equals(data)){
+                head = head.Next;
+                count--;
+                return true;
+            }
+            Node<T> current = head;
+            while (current.Next != null && !current.Next.Data.Equals(data)){
+                current = current.Next;
+            }
+            if (current.Next == null){
+                return false;
+            }
+            current.Next = current.Next.Next;
+            count--;
+            return true;
+        }
+        public T GetIndex(int index){
+            if (index < 0 || index >= count){
+                throw new ArgumentOutOfRangeException("Індекса не існує");
+            }
+            Node<T> current = head;
+            for (int i = 0; i < index; i++){
+                current = current.Next;
+            }
+            return current.Data;
+        }
+        public bool Exist(T data){
+            Node<T> current = head;
+            while (current != null){
+                if (current.Data.Equals(data)){
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+        public int Count{
+            get{
+                return count;
+            }
+        }
+        public void Print(){
+            Node<T> current = head;
+            Console.Write("Список: ");
+            while (current != null){
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+        public void Reverse(){
+            Node<T> prev = null; Node<T> current = head; Node<T> next = null; while (current != null){
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+        }
+    }
+}
---
+++ SingleList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace SingleList{
+    internal class Program{
+        static void Main(string[] args){
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            SingleList<int> list = new SingleList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Print();
+            Console.WriteLine("Видалення елемента 2: " + list.Remove(2));
+            list.Print();
+            Console.WriteLine("Елемент 3 існує в списку: " + list.Exist(4));
+            Console.WriteLine("Елемент 2 існує в списку: " + list.Exist(2));
+            list.Reverse();
+            list.Print();
+            Console.WriteLine("Елемент по индексу 1: " + list.GetIndex(1));
+            Console.WriteLine("Кількість елементів: " + list.Count);
+        }
+    }
+}
